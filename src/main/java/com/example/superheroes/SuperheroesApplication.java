ac088989947e1a4dee565d76a039469874de6af0
renatu <renatu_mb@yahoo.com.br>
2023-07-12 22:46:07 -0700

    configs

--- src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuperheroesApplication {
+public class SuperHeroesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SuperheroesApplication.class, args);
+        SpringApplication.run(SuperHeroesApplication.class, args);
     }
-
 }
